544de38cc9b208716c95d1864baf5a45df051b88
Prafull Sharma <prafull2001@Prafulls-MacBook-Air.local>
2016-10-25 19:37:08 -0400

    Loaded Box2D physics engine, imported Ninja and gave it a physical body

--- main.lua
+++ main.lua
@@ -1,13 +1,26 @@
 -----------------------------------------------------------------------------------------
 --
 -- main.lua
---
------------------------------------------------------------------------------------------
 
--- Your code here
 
+-- we'll be working from 'main.lua' for now, research to be done on using classes...
+
+
+--creating instances of each class, ignore for now
 local hero = require("hero")
 local obstacle = require("obstacle")
 local environment = require("environment")
 
+local ninja = display.newImage("Ninja.JPG", 50, 250)
+ninja:scale(.08, .08)
 
+
+-- load Box2D physics engine 
+local Physics = require("physics")
+
+-- start engine
+Physics.start()
+
+
+-- give ninja physical body 
+Physics.addBody(ninja, "dynamic", {radius = 50, bounce = 0.3})
