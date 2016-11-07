2954ac90f113d2921ed47929798b235c397045f8
Prafull Sharma <prafull2001@Prafulls-MacBook-Air.local>
2016-11-06 19:14:36 -0500

    Added Jump functionality

--- main.lua
+++ main.lua
@@ -16,19 +16,18 @@ local ninja = display.newImageRect("Ninja.JPG", 56, 80)
 ninja.x = 75
 ninja.y = 250
 
-
+--giving ninja a physical body...
 Physics.addBody(ninja, "dynamic", {radius = 50, bounce = 0.3})
 
-local function jump()
+local function Jump()
 	ninja:applyLinearImpulse(0, -0.75, ninja.x, ninja.y)
-	--Applying 0 force in the X direction and -0.75 in the Y direction. Y is negative bc down is considered positive by the physics engine.
-	--Apply force to the center of the balloon, hence balloonX & balloonY.
-	--Modifies only the TEXT PROPERTY of tapText object (increases counter)
+	--Applying 0 force in the X direction and -0.75 in the Y direction. Y is negative because down is considered positive by the physics engine.
+	--Apply force to the center of the balloon, hence ninja.x & ninja.y
 end
 
 
 
-ninja:addEventListener("tap", jump)
+ninja:addEventListener("tap", Jump)
 
 
 
