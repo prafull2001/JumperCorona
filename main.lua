2ef7fa5abd5b4d46b4494d04514c3d79a32b2df9
Prafull Sharma <prafull2001@Prafulls-MacBook-Air.local>
2016-11-11 15:11:11 -0500

    Added sky and grass, only temp sprites...

--- main.lua
+++ main.lua
@@ -29,9 +29,11 @@ Physics.addBody(grass, "static", {bounce = 0})
 Physics.addBody(ninja, "dynamic", {radius = 50, bounce = 0})
 
 local function Jump()
+
+	--work on decreasing air time by increasing gravity...
 	ninja:applyLinearImpulse(0, -0.4, ninja.x, ninja.y)
 	--Applying 0 force in the X direction and -0.75 in the Y direction. Y is negative because down is considered positive by the physics engine.
-	--Apply force to the center of the balloon, hence ninja.x & ninja.y
+	--Apply force to the center of the ninja, hence ninja.x & ninja.y
 end
 
 
