790adf756df2e9aaeb41eedf21344df79b9726a9
Prafull Sharma <prafull2001@Prafulls-MacBook-Air.local>
2016-11-12 19:03:43 -0500

    Added animation, sky, and grass

--- main.lua
+++ main.lua
@@ -12,11 +12,13 @@ local Physics = require("physics")
 Physics.start()
 
 
+
+
 local sky = display.newImage( "sky.jpg", 250, 75 )
 sky:scale( 1.5, 1.5 )
 
-local grass = display.newImage( "grass.png", 197, 300)
 
+local grass = display.newImage( "grass.png", 197, 300)
 
 
 local ninja = display.newImageRect("Ninja.JPG", 60 , 80)
@@ -27,6 +29,21 @@ Physics.addBody(grass, "static", {bounce = 0})
 
 --giving ninja a physical body...
 Physics.addBody(ninja, "dynamic", {radius = 20, bounce = 0})
+
+
+
+
+local greenGuysheet = graphics.newImageSheet( "greenman.png", { width=128, height=128, numFrames=15 } )
+
+-- play 15 frames every 500 ms
+local instance2 = display.newSprite( greenGuysheet, {start=1, count=15, time=500 } )
+instance2.x = 200
+instance2.y = 260
+instance2:play()
+
+
+
+
 
 local function Jump()
 
